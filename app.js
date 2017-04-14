fe271127355c8ca345e6639ad8106022f00085e8
xiaochuan <328190650@qq.com>
2017-04-14 17:40:28 +0800

    4.14  17:40

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 
         /* 1、创建组件 */
         const Home = Vue.extend({
-            template: '<div><h1>Home</h1><p>{{msg}}</p></div>',
+            template: '<div><h1>Hello</h1><button v-on:click="greet">Greet</button></div>',
             data: function() {
                 return {
                     msg: 'Hello, vue router!'
                 }
             },
-            watch:{'$route'(to,from){
-            		console.log("ss");
+            methods:{
+              greet:function(){
+                router.push({ path: '/user/foo/posts' })
               }
             }
-        });
-        const About = Vue.extend({
-            template: '<div><h1>About</h1><p>{{$route.params.grade}}--{{$route.params.number}}</p></div>'
-        });
+        })
+        const User = Vue.extend({
+            template: '<div class="user"><h2>about</h2><router-view></router-view></div>'
+        })
+        const UserHome = { template: '<div>UserHome</div>' }
+        const UserProfile = { template: '<div>UserProfile</div>' }
+        const UserPosts = { template: '<div>UserPosts</div>' }
 
-        // 2. 创建 router 实例，然后传 `routes`路由映射 配置
+        // 2. 创建 router 实例，然后传 `routes` 路由映射 配置
         const router = new VueRouter({
           routes: [
-            { path: '/home', component: Home },
-              { path: '/about/:grade/:number', component: About },
+              { path: '/home',name: 'home', component: Home },
+              {  path: '/user/:userId',name: 'user',component: User,
+                 children: [
+                    { path: '', component: UserHome },
+                    { path: 'profile', component: UserProfile },
+                    { path: 'posts', component: UserPosts }
+                  ]
+              },
               { path: '/', component: Home } //设置默认路径
           ] 
         });

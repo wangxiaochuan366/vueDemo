d18fda5a385d4e866027753ff307db4d71b96a7a
xiaochuan <328190650@qq.com>
2017-04-26 17:33:31 +0800

    命名试图

--- app.js
+++ app.js
@@ -1,40 +1,53 @@
 const Foo = { template:'<div>foo</div>'}
 const Bar = {template:'<div>bar</div>'}
+const Baz = {template:'<div>baz</div>'}
+const User = {template:'<div>user{{$route.query}}</div>'}
 const UserHome = {template:'<div>UserHome</div>'}
 const UserPosts = {template:'<div>UserPosts</div>'}
 const UserProfile = {template:'<div>UserProfile</div>'}
-const User = {template:'<div>User</div>'}
-/*const User = {
-  template: `
-    <div class="user">
-      <h2>User {{ $route.params.id }}</h2>
-      <router-view></router-view>
-    </div>
-  `
-}*/
+const NotFoundComponent = {template:'<div>NotFoundComponent</div>'}
+
 const routes =[
-   {path:'/foo',component:Foo,name:'foo'},
-   {path:'/bar',component:Bar,name:'bar'},
-/*   {
-      path: '/user/:id', component: User,
-      children: [
-        // 当 /user/:id 匹配成功，
-        // UserHome 会被渲染在 User 的 <router-view> 中
-        { path: '', component: UserHome },
-        { path: 'profile', component:UserPosts,name:'profile'},
-        { path: 'posts', component:UserProfile,name:'posts'}
-      ]
-    }*/
-      {
-      path: '/user/:userId',
-      name: 'user',
-      component: User
-    }
+   {path:'/user/:userId',component:User,name:'user'},
+   {path:'/foo',component:Foo},
+   {path:'/bar',component:Bar},
 ]
 
 
 // 3. 创建 router 实例，然后传 `routes` 配置
 // 你还可以传别的配置参数, 不过先这么简单着吧。
 const router = new VueRouter({
-  routes // （缩写）相当于 routes: routes
+  /*hash模式  URL改变时，页面不会重新加载
+  通过history完成 URL 跳转而无须重新加载页面。
+  */
+  /* mode:'history',*/
+   routes: [
+    {
+      path: '/',
+      components: {
+        default: Foo,
+        a: Bar,
+        b: Baz
+      }
+    }
+  ]
 })
+
+/* 编程式的导航
+mode:  hash模式  默认
+mode:  history模式
+router.push(location)  history中会有记录
+router.replace(location)  hisstory中不会有记录
+router.go(n)   在history中向前跳转或者向后跳转
+
+param   #/user/123
+query   #/?userid=123
+*/
+
+/*
+命名视图
+就是一个页面可以有多个视图，
+一个视图对应一个组件。一个路由，
+n个视图n个组件，
+相当于一次路由展示了多个组件。
+*/

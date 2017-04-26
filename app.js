13325ed029eb8b2c0f70cc39157594d5b68307d5
xiaochuan <328190650@qq.com>
2017-04-26 11:36:35 +0800

    命名路由

--- app.js
+++ app.js
@@ -3,26 +3,32 @@ const Bar = {template:'<div>bar</div>'}
 const UserHome = {template:'<div>UserHome</div>'}
 const UserPosts = {template:'<div>UserPosts</div>'}
 const UserProfile = {template:'<div>UserProfile</div>'}
-const User = {
+const User = {template:'<div>User</div>'}
+/*const User = {
   template: `
     <div class="user">
       <h2>User {{ $route.params.id }}</h2>
       <router-view></router-view>
     </div>
   `
-}
+}*/
 const routes =[
-   {path:'/foo',component:Foo},
-   {path:'/bar',component:Bar},
-   {
+   {path:'/foo',component:Foo,name:'foo'},
+   {path:'/bar',component:Bar,name:'bar'},
+/*   {
       path: '/user/:id', component: User,
       children: [
         // 当 /user/:id 匹配成功，
         // UserHome 会被渲染在 User 的 <router-view> 中
         { path: '', component: UserHome },
-        { path: 'profile', component:UserPosts},
-        { path: 'posts', component:UserProfile}
+        { path: 'profile', component:UserPosts,name:'profile'},
+        { path: 'posts', component:UserProfile,name:'posts'}
       ]
+    }*/
+      {
+      path: '/user/:userId',
+      name: 'user',
+      component: User
     }
 ]
 

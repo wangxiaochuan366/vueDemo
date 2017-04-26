80ad9fc581e9c5b4a53826e45b8574d4fff5f45d
xiaochuan <328190650@qq.com>
2017-04-26 11:10:11 +0800

    嵌套路由

--- app.js
+++ app.js
@@ -1,38 +1,34 @@
-
-        /* 1、创建组件 */
-        const Home = Vue.extend({
-            template: '<div><h1>Hello</h1><button v-on:click="greet">Greet</button></div>',
-            data: function() {
-                return {
-                    msg: 'Hello, vue router!'
-                }
-            },
-            methods:{
-              greet:function(){
-                router.push({ path: '/user/foo/posts' })
-              }
-            }
-        })
-        const User = Vue.extend({
-            template: '<div class="user"><h2>about</h2><router-view></router-view></div>'
-        })
-        const UserHome = { template: '<div>UserHome</div>' }
-        const UserProfile = { template: '<div>UserProfile</div>' }
-        const UserPosts = { template: '<div>UserPosts</div>' }
-
-        // 2. 创建 router 实例，然后传 `routes` 路由映射 配置
-        const router = new VueRouter({
-          routes: [
-              { path: '/home',name: 'home', component: Home },
-              {  path: '/user/:userId',name: 'user',component: User,
-                 children: [
-                    { path: '', component: UserHome },
-                    { path: 'profile', component: UserProfile },
-                    { path: 'posts', component: UserPosts }
-                  ]
-              },
-              { path: '/', component: Home } //设置默认路径
-          ] 
-        });
+const Foo = { template:'<div>foo</div>'}
+const Bar = {template:'<div>bar</div>'}
+const UserHome = {template:'<div>UserHome</div>'}
+const UserPosts = {template:'<div>UserPosts</div>'}
+const UserProfile = {template:'<div>UserProfile</div>'}
+const User = {
+  template: `
+    <div class="user">
+      <h2>User {{ $route.params.id }}</h2>
+      <router-view></router-view>
+    </div>
+  `
+}
+const routes =[
+   {path:'/foo',component:Foo},
+   {path:'/bar',component:Bar},
+   {
+      path: '/user/:id', component: User,
+      children: [
+        // 当 /user/:id 匹配成功，
+        // UserHome 会被渲染在 User 的 <router-view> 中
+        { path: '', component: UserHome },
+        { path: 'profile', component:UserPosts},
+        { path: 'posts', component:UserProfile}
+      ]
+    }
+]
 
 
+// 3. 创建 router 实例，然后传 `routes` 配置
+// 你还可以传别的配置参数, 不过先这么简单着吧。
+const router = new VueRouter({
+  routes // （缩写）相当于 routes: routes
+})
